0d0a6c99d2d45e9a208bdb5499718653eefeba0c
tristanmietle <112154856+tristanmietle@users.noreply.github.com>
2026-03-19 14:57:05 +0100

    fikset på kommentarer før innlevering.

--- source/main.c
+++ source/main.c
@@ -5,42 +5,12 @@
 #include "driver/elevio.h"
 
 
-//none lys(etasjer?){}:
-//kjøres kontinuerlig
-//må kunne vite om heisen stopper i en etasje, for å slukke lyset til knappen utenfor (when stopped light toggle off).
-
-//none door_open(etg){}: kalles fra bestillinger, muligens obst eller stopp
-//åpne (lampe og var), timer 3 sek, lukke
-//sjekke at heis står stille, sjekke at heis er i en etasje
-
-
-//bool? stop_btn(){}: kalles når stoppknappen trykkes, (veldig) mulig denne må kalles i door open
-//her må man finne en lur løsning på hvordan man sjekker at heisen er i en etasje, og ikke mellom, sjekke sensorverdier
-//skal være gyldig så lenge stoppknappen er true
-//skal få lyset til å lyse, må sjekkes opp mot door open etter eller annet sted
-//må sette motorspeed til 0 
-//sette alle orders til false
-//skal ikke være mulig å gjøre bestillinger, mulig en konsekvens av å sette orders til false
-
-//bool? obs(){}: enklere stopp, mulig det er overflødig med egen funksjon, er bare en iftest om bryer og dør er true
-//etter bryter blir false, kalle timern fra dør, gå videre
-//MÅ ligge under dørene
-//
-
 int orders[4][4] = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}}; //første i lista er om heisen skal stoppe, andre er om retningen har noe å si - knapper på panel utenfor vs inni
 //orders[i][1] er begge, orders[i][2] er opp, orders[i][3] er ned 
 int orderChanged = 1;
-int heading = 1; //samme som orders, settes til 1 fordi heisen skal være i 1. etg ved oppstart
+int heading = 1; //settes til 1 fordi heisen skal være i 1. etg ved oppstart
 int doorOpen = 0;
 int lastFloor = 0;
-
-//må sannsynligvis ha tilgang på lysinputs, bruke de til å toggle lista
-//none bestillinger(){}:
-//bruker en liste for å sjekke hvor den stopper, sjekker dette opp mot motorretning og nåværende posisjon (skal ikke snu hvis det finnes fler bestillinger i retningen den beveger seg i) 
-//hvis heisen stopper i en etasje skal etasjen fjærnes/settes til false i lista
-//hvis liste tom => motorspeed = 0
-//kommer til å kunne fjerne true fra lys når bestillinger utføres
-
 
 int ordersEmpty(){ //sjekker om ordrene er tomme
     for(int i = 0; i < N_FLOORS; i++){
@@ -58,7 +28,7 @@ void clearOrderRow(int row){ //tømmer en rad
     orderChanged = 1;
 }
 
-void panelLights(){ // mulig denne kan endres og kalles ved bestilling
+void panelLights(){ //fikser lysene til panelene
     if (orderChanged){ 
         for (int i = 0; i < N_FLOORS; i++){
             for (int b = 0; b < N_BUTTONS; b++){
@@ -79,12 +49,10 @@ void panelLights(){ // mulig denne kan endres og kalles ved bestilling
     }
 }
 
-void settBestillinger(){ //tilsvarer getPressedButtons() i UML, bytte om til funksjon for å hente alle pressed buttons? 
-    //sjekker panelknapper, må muligens skrives om
+void settBestillinger(){ //sjekker om knappene er trykket inn, og endrer på orders hvis de er det
     for (int i = 0; i < N_FLOORS; i++){ 
         for (int b = 0; b < N_BUTTONS; b++){
             int btnPressed = elevio_callButton(i, b);
-            //lite elegant
             if (btnPressed & b == 2){ //sjekker knappene inni heisen
                 orders[i][0] = 1;
                 orders[i][1] = 1;
@@ -102,7 +70,7 @@ void settBestillinger(){ //tilsvarer getPressedButtons() i UML, bytte om til fun
     }
 }
 
-void settHeading(int etg){
+void settHeading(int etg){ //finner ut hviken retning heisen skal bevege seg i
     int ordersAbove = 0;
     int ordersBelow = 0;
     if (etg == 3){
@@ -136,27 +104,26 @@ void settHeading(int etg){
     }
 }
 
-void bevegelse(){ //kutte ut?
+void bevegelse(){ // gjør at heisen beveger seg.
     if (ordersEmpty()){
         elevio_motorDirection(DIRN_STOP);
     } else{
         elevio_motorDirection(heading);
     }
-    doorOpen = 0; //ER DENNE NØDVENDIG???
+    doorOpen = 0;
 }
 
-void openDoor(){
+void openDoor(){ //åpner dør
     doorOpen = 1;
     elevio_doorOpenLamp(1);
 }
 
-void closeDoor(){
+void closeDoor(){ //lukker dør
     doorOpen = 0;
     elevio_doorOpenLamp(0);
 }
 
-
-void waitDoor(){
+void waitDoor(){ //venter i tre sekunder, sjekker om det legges inn bestillinger og oppdaterer lys
     time_t start = time(NULL);
     elevio_motorDirection(DIRN_STOP);
     while (time(NULL) - start < 3){
@@ -166,7 +133,7 @@ void waitDoor(){
     }
 }
 
-void obstruksjon(){
+void obstruksjon(){ //håndterer obstruksjonsknappen
     int obstruksjon = 0;
     elevio_stopLamp(0);
     while((elevio_obstruction()) & (doorOpen == 1)){
@@ -182,8 +149,7 @@ void obstruksjon(){
     
 }
 
-
-void stoppKnapp(int etg){
+void stoppKnapp(int etg){ //hådterer stoppknappen.
     
     if (elevio_stopButton()){
         elevio_stopLamp(1);
@@ -214,13 +180,9 @@ void stoppKnapp(int etg){
     } else {
         elevio_stopLamp(0);
     }
-    
-    
-    
 }
 
-
-void stopInFloor(int etg){
+void stopInFloor(int etg){ //sjekker om heisen skal toppe i en etasje
     int orderAmount = 0;
     for (int i = 0; i < N_FLOORS; i++){
         if (orders[i][0]){
@@ -255,42 +217,22 @@ void stopInFloor(int etg){
             clearOrderRow(etg);
         }
     }
-    // if ((orders[etg][0] == 1) & ((orders[etg][1] == 0) || (orders[etg][1] == heading))){
-    //     elevio_doorOpenLamp(1);
-    //     time_t start = time(NULL);
-    //     clearOrderRow(etg);
-    //     elevio_motorDirection(DIRN_STOP);
-    //     while (time(NULL) - start < 3);
-    //     elevio_doorOpenLamp(0);
-    // }
 }
 
-void floorLights(){ //Bytte til setPanelLights() ?
+void floorLights(){ //håndterer lysene som viser hvilken etasje heisen er i
     if (elevio_floorSensor() >= 0){
             elevio_floorIndicator(elevio_floorSensor());
     }
 }
 
-
-
-
-
 int main(){
-    //Initialisering
     elevio_init();
-
-
-    //printf("=== Example Program ===\n");
-    //(printf("Press the stop button on the elevator panel to exit\n");
-
-    //Oppstart
     elevio_motorDirection(DIRN_DOWN);
     int oppstart_bool = 0;
 
     while(1){
-
-        if (!oppstart_bool){
-            panelLights(); //mulig feilkilde
+        if (!oppstart_bool){ // oppstart
+            panelLights();
             floorLights();
             if(elevio_floorSensor() == 0){
                 oppstart_bool = 1;
@@ -299,9 +241,7 @@ int main(){
         }
         //Program-loop
         else {
-            //mulig btnlirDirection(DIRN_UP);
-            //if(floor == 0ghts()?
-            int floor = elevio_floorSensor(); //sjekke verdier her, 0-3 eller 1-4?
+            int floor = elevio_floorSensor();
     
             settBestillinger();
             settHeading(lastFloor);
@@ -314,64 +254,9 @@ int main(){
                 obstruksjon();
                 lastFloor = floor;
             }
-            
-            
-            //muig dette gjøres om til funksjon
-            /*if (ordersEmpty()){
-                elevio_motorDirection(DIRN_STOP);
-            }
-            else {
-                elevio_motorDirection(DIRN_UP);
-            }*/
-            //if(floor == 0){
-            //    elevio_motorDirection(DIRN_UP);
-            //}
-
-            //if(floor == N_FLOORS-1){
-            //    elevio_motorDirection(DIRN_DOWN);
-            //}
-
-
-            //sjekker at knappene lyser når de blir trykket på, mulig en liknende variant for å håndtere bestillinger
-            //for(int f = 0; f < N_FLOORS; f++){
-            //    for(int b = 0; b < N_BUTTONS; b++){
-            //        int btnPressed = elevio_callButton(f, b);
-            //        elevio_buttonLamp(f, b, btnPressed);
-            //    }
-            //}
-            
-           
-            // if(elevio_obstruction()){
-            //     elevio_stopLamp(1);
-            // } else {
-            //     elevio_stopLamp(0);
-            // }
-        
         }
-        //oppstartskode, typ send ned til 1. etg
-        //sett retning, sjekk etg, ikke bruk dører eller obs (tror jeg), 
-        //husk at heisknappene må funke, lage btnlights()?
-
-       
-        // if(elevio_stopButton()){
-        //     elevio_motorDirection(DIRN_STOP);
-        //     break;
-        // }
-        
         nanosleep(&(struct timespec){0, 20*1000*1000}, NULL);
     }
 
     return 0;
 }
-
-
-
-
-
-/*
-Forslag:
-lage oppsart til en egen funksjon
-Lage program-loop eneklt i main()
-
-
-*/
